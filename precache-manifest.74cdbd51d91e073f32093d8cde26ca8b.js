9c01142f4eb0e46758f13ba4b4c2a09655c7408c
Krishna chaitanya Vulchi <chaitanyamean@gmail.com>
2019-10-04 16:25:51 +0530

    Updates

---
+++ precache-manifest.74cdbd51d91e073f32093d8cde26ca8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd151b46065a46c2a199e29affce433e",
+    "url": "/monster-react/index.html"
+  },
+  {
+    "revision": "67f5644d2d21c1ae81f0",
+    "url": "/monster-react/static/css/main.4e228d17.chunk.css"
+  },
+  {
+    "revision": "7d0e9689fbedf7f79dd6",
+    "url": "/monster-react/static/js/2.d312d690.chunk.js"
+  },
+  {
+    "revision": "67f5644d2d21c1ae81f0",
+    "url": "/monster-react/static/js/main.1391e7bd.chunk.js"
+  },
+  {
+    "revision": "ec8fda2a38237c444e39",
+    "url": "/monster-react/static/js/runtime-main.5e0b8d84.js"
+  }
+]);
